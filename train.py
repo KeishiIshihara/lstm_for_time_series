7ab33ef5e43ace3c58e26c9b5fc20012a119d18c
KeishiIshihara <ishihara@aisl.cs.tut.ac.jp>
2019-09-30 15:27:27 +0300

    update: clean entire looking

--- train.py
+++ train.py
@@ -1,10 +1,6 @@
 #============================================================
 # LSTM for time series dataset (household power consumption)
 #
-# TODO: 
-#  - picklize data
-#  - training curve
-# 
 #    (c) Keishi Ishihara
 #============================================================
 from __future__ import print_function
@@ -28,17 +24,15 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('-d','--debug',action='store_true',default=False, help='Execute with debug mode')
+    parser.add_argument('-e','--epochs',default=10,type=int, help='Epoch')
     parser.add_argument('--test-size',default=0.1, help='Test size from 0 to 1. Default is 0.2')
     parser.add_argument('--not-record', '-nr', action='store_true', default=False)
-    parser.add_argument('--time_steps',default=20, help='Time steps in each sequence data')
-
+    parser.add_argument('-t', '--time_steps',default=20, type=int, help='Time steps in each sequence data')
     args = parser.parse_args()
 
     #============================
     #         Configs
     #============================
-    logdir = './log' 
-
     length_of_sequence = args.time_steps # time steps
     in_neurons = 7
     out_neurons = 1
@@ -48,80 +42,61 @@ if __name__ == '__main__':
     #          Model
     #============================
     lstm = LSTM(n_hidden,
-                # batch_input_shape=(None, length_of_sequence, in_neurons),
+                activation='tanh',
                 input_shape=(length_of_sequence, in_neurons),
                 dropout=0.2,
                 recurrent_dropout=0.2, 
-                return_sequences=False)
+                return_sequences=False
+                )
 
     model = Sequential()
     model.add(lstm)
     model.add(Dense(units=out_neurons, activation='linear'))
     model.compile(loss="mean_squared_error",
-                  optimizer=Adam(lr=0.01),
-                  metrics=['accuracy'])
+                  optimizer=Adam(lr=0.001)
+                #   metrics=['accuracy']
+                  )
     model.summary()
 
     # Load data
     (X_train, X_test), (y_train, y_test) = load_time_series(debug=args.debug, 
                                                             test_size=args.test_size,
-                                                            maxlen=args.time_steps)
+                                                            maxlen=length_of_sequence)
 
     #============================
     #         Callbacks
     #============================
-    if not args.not_record:
-        # ms_cb = CILModelSave(args.output_dir_path, prefix) # for saving model
-        # es_cb = EarlyStopping(monitor='val_loss', mode='auto', patience=20)
-        tb_cb = keras.callbacks.TensorBoard(log_dir=logdir)
-        # callbacks = [ms_cb, tb_cb, es_cb]
-        callbacks = [tb_cb]
-    else:
-        callbacks = None
+    es_cb = EarlyStopping(monitor='val_loss', mode='auto', patience=20)
+    callbacks = [es_cb]
 
     #============================
     #         Training
     #============================
-    print(X_train.shape)
-    for i in range(10):
-        print('x_train {}, y_train {}'.format(X_train[i,:,0], y_train[i]))
-
     history = model.fit(X_train, y_train,
-            batch_size=64,
-            epochs=10,
-            # callbacks=callbacks,
-            validation_data=(X_test,y_test),
-            verbose=1 # 2
+            batch_size=256,
+            epochs=args.epochs,
+            callbacks=callbacks,
+            verbose=1, # 2
+            validation_split=0.2
             )
 
     #============================
-    #         Evaluation
+    #         Prediction
     #============================
-    X_test = X_test[:20]
-    y_test = y_test[:20]
-    score_train = model.evaluate(x=X_train, y=y_train, verbose=1)
-    score_test = model.evaluate(x=X_test, y=y_test, verbose=1)
-    print('Train: loss={},accuracy={}'.format(score_train[0],score_train[1]))
-    print('Test: loss={},accuracy={}'.format(score_test[0],score_test[1]))
-
-    predicted = model.predict(X_test, batch_size=None, verbose=0, steps=None)
-    print(X_test.shape)
-    for i in range(len(predicted)):
-        print('x_test {}, predict {}, y_test{}'.format(X_test[i,:,0], predicted[i], y_test[i]))
+    X_test = X_test[500:700]
+    y_test = y_test[500:700]
+    predicted = model.predict(X_test)
 
     #=============================
     #     Draw training curves
     #=============================
     fig = plt.figure() # Initialize Figure
     ax = fig.add_subplot(1,1,1)
-    ax.plot(range(len(predicted)), predicted, color="orange", label="predict_data")
     ax.plot(range(len(y_test)), y_test, color="blue", label="ground_truth")
-    ax.plot(range(len(y_test)), X_test[:,:,0], color="green", label="previous_ts")
+    ax.plot(range(len(predicted)), predicted, color="red", label="predict_data")
     ax.legend(loc='best')
-    ax.set_xticks(range(0,len(X_test)-1))
-    # ax.set_xticklabels()
-    # ax.grid(which='both')
-    ax.set_xlabel('time step')
+    ax.grid(which='both')
+    ax.set_xlabel('Time')
     ax.set_ylabel('Global_active_power')
-    fig.savefig('result.png')
+    fig.savefig('results/result.png')
     plt.close()
